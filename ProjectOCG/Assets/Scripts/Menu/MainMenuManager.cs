7ef4b234d500d4aa930f04aa8aadf357d132abc0
EfendiKilic <113355785+EfendiKilic@users.noreply.github.com>
2025-12-13 19:49:08 +0300

    RoadToMenu/3

--- ProjectOCG/Assets/Scripts/Menu/MainMenuManager.cs
+++ ProjectOCG/Assets/Scripts/Menu/MainMenuManager.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject SettingsPanel;
-    
+
+    public void OpenSettings()
+    {
+        
+    }
+
+    public void CloseSettings()
+    {
+        
+    }
     
 }
